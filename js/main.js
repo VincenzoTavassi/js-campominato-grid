265c8502a5fe5cb242208357bf1b5e4518a6f9ad
VincenzoTavassi <info@egadivacanze.it>
2023-02-01 16:10:21 +0100

    bonus final commit

--- js/main.js
+++ js/main.js
@@ -14,11 +14,31 @@
 // DEFINE HTML ELEMENTS 
 const gridEl = document.getElementById('grid');
 const startButtonEl = document.getElementById('start-game');
+const resetButtonEl = document.getElementById('reset-game');
+// const difficultyEl = document.getElementById('difficulty').value;
+// console.log(difficultyEl);
 
-// BUTTON EVENT LISTENER 
-startButtonEl.addEventListener('click', function () {
-    generateGrid(gridEl, 100);
+// BUTTON EVENT LISTENERS 
+
+// start game button 
+startButtonEl.addEventListener('click',
+    function () {
+        const difficultyEl = document.getElementById('difficulty').value;
+        let difficultyValue = 1;
+        if (difficultyEl == 'medium') {
+            difficultyValue = 2;
+        } else if (difficultyEl == 'hard') {
+            difficultyValue = 3;
+        }
+        generateGrid(gridEl, difficultyValue);
+    });
+
+// reset game button 
+resetButtonEl.addEventListener('click', function () {
+    gridEl.innerHTML = '';
 })
+
+
 
 /*****************************************************
  *                                                   *
@@ -28,13 +48,39 @@ startButtonEl.addEventListener('click', function () {
 
 // generateGrid(gridEl, 100)
 
-function generateGrid(grid, dimension) {
+/****************************************************
+ * 
+ * Generates a grid based on the difficulty level 
+ * ( 1 = easy, 2 = medium, 3 = hard);
+ * 
+ * @param {HTMLElement} grid the HTML Element of the grid
+ * @param {int} difficulty a number betwen 1, 2 and 3
+ * 
+ * 
+****************************************************/
+
+function generateGrid(grid, difficulty) {
     // reset grid 
     grid.innerHTML = ''
+    // STABILISCO LIVELLO DI DIFFICOLTA'
+    let squareClass;
+    let gridDimension;
+    if (difficulty == 1) {
+        gridDimension = 100;
+        squareClass = 'easy'
+    } else if (difficulty == 2) {
+        gridDimension = 81;
+        squareClass = 'medium'
+    } else if (difficulty == 3) {
+        gridDimension = 49;
+        squareClass = 'hard'
+    }
+
     // GENERO UN NUMERO DI QUADRATI PARI ALLA DIMENSIONE RICHIESTA 
-    for (let i = 0; i < dimension; i++) {
+    for (let i = 0; i < gridDimension; i++) {
         let newSquareEl = document.createElement('div');
-        newSquareEl.classList.add('square');
+        // AGGIUNGO QUADRATO CON CLASSE IN BASE ALLA DIFFICOLTA' 
+        newSquareEl.classList.add('square', squareClass);
         // INSERISCO NUMERO DEL QUADRATO 
         newSquareEl.append(i + 1);
         // AGGIUNGO QUADRATO ALLA GRIGLIA 
